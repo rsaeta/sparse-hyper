208c3217dd3dec4ea034c757064be1e1e08853f6
Ryan Saeta <rsaeta@gmail.com>
2023-06-26 11:48:14 +0200

    Black everything

--- spalbp/lib/attention/clustered_attention.py
+++ spalbp/lib/attention/clustered_attention.py
@@ -1,37 +1,41 @@
 import torch
 from torch import nn
-# from routing_transformer.routing_transformer import SelfAttention
 from routing_transformer import KmeansAttention
 from .config import ClusteredAttentionConfig
 
 
 class ClusteredAttention(nn.Module):
-
     @classmethod
     def from_config(cls, config: ClusteredAttentionConfig):
-        return cls(config.heads,
-                   config.emb,
-                   config.context,
-                   config.num_clusters,
-                   config.head_size,
-                   config.attention_dropout,
-                   config.window_size)
+        return cls(
+            config.heads,
+            config.emb,
+            config.context,
+            config.num_clusters,
+            config.head_size,
+            config.attention_dropout,
+            config.window_size,
+        )
 
-    def __init__(self,
-                 num_heads: int,
-                 embedding_dim: int,
-                 context: int,
-                 clusters: int,
-                 head_size: int,
-                 dropout: float,
-                 window_size: int):
+    def __init__(
+        self,
+        num_heads: int,
+        embedding_dim: int,
+        context: int,
+        clusters: int,
+        head_size: int,
+        dropout: float,
+        window_size: int,
+    ):
         super().__init__()
-        self.attention = KmeansAttention(clusters,
-                                         window_size,  # window_size
-                                         4,  # global attention
-                                         head_size,
-                                         causal=False,
-                                         dropout=dropout)
+        self.attention = KmeansAttention(
+            clusters,
+            window_size,  # window_size
+            4,  # global attention
+            head_size,
+            causal=False,
+            dropout=dropout,
+        )
         self.num_heads = num_heads
         self.head_size = head_size
         self.to_keys = nn.Linear(embedding_dim, head_size * num_heads)
@@ -41,9 +45,21 @@ class ClusteredAttention(nn.Module):
 
     def forward(self, x: torch.Tensor, attention_mask: torch.Tensor):
         b, c, e = x.shape
-        k = self.to_keys(x).view(b, c, self.num_heads, self.head_size).transpose(1, 2)  # (B, nh, T, hs)
-        v = self.to_values(x).view(b, c, self.num_heads, self.head_size).transpose(1, 2)  # (B, nh, T, hs)
-        q = self.to_queries(x).view(b, c, self.num_heads, self.head_size).transpose(1, 2)  # (B, nh, T, hs)
-        out, loss = self.attention(q, k, v)  # kind of annoying have to deal with aux-loss here
-        out = self.unify(out.transpose(1, 2).reshape(b, c, self.num_heads * self.head_size))
+        k = (
+            self.to_keys(x).view(b, c, self.num_heads, self.head_size).transpose(1, 2)
+        )  # (B, nh, T, hs)
+        v = (
+            self.to_values(x).view(b, c, self.num_heads, self.head_size).transpose(1, 2)
+        )  # (B, nh, T, hs)
+        q = (
+            self.to_queries(x)
+            .view(b, c, self.num_heads, self.head_size)
+            .transpose(1, 2)
+        )  # (B, nh, T, hs)
+        out, loss = self.attention(
+            q, k, v
+        )  # kind of annoying have to deal with aux-loss here
+        out = self.unify(
+            out.transpose(1, 2).reshape(b, c, self.num_heads * self.head_size)
+        )
         return out, loss
--- spalbp/lib/models/native_transformer.py
+++ spalbp/lib/models/native_transformer.py
@@ -16,32 +16,35 @@ class NativeTransformerConfig:
 
 
 class NativeTransformer(nn.Module):
-
     @classmethod
     def from_config(cls, config: NativeTransformerConfig):
         return cls(**config.__dict__)
 
-    def __init__(self,
-                 emb: int,
-                 heads: int = 4,
-                 ff_hidden_mult: int = 4,
-                 dropout: float = 0.0,
-                 depth: int = 0,
-                 context: int = 0,
-                 vocab: int = 32000):
+    def __init__(
+        self,
+        emb: int,
+        heads: int = 4,
+        ff_hidden_mult: int = 4,
+        dropout: float = 0.0,
+        depth: int = 0,
+        context: int = 0,
+        vocab: int = 32000,
+    ):
         super().__init__()
 
         self.token_embedding = nn.Embedding(num_embeddings=vocab, embedding_dim=emb)
         self.pos_embedding = Summer(PositionalEncoding1D(emb))
         self.nheads = heads
         self.context = context
-        encoder_layer = nn.TransformerEncoderLayer(emb,
-                                                   heads,
-                                                   ff_hidden_mult * emb,
-                                                   dropout,
-                                                   batch_first=True,
-                                                   norm_first=True,
-                                                   device='cuda')
+        encoder_layer = nn.TransformerEncoderLayer(
+            emb,
+            heads,
+            ff_hidden_mult * emb,
+            dropout,
+            batch_first=True,
+            norm_first=True,
+            device="cuda",
+        )
         self.transformer = nn.TransformerEncoder(encoder_layer, depth)
         self.to_vocab = nn.Linear(emb, vocab)
 
@@ -52,6 +55,10 @@ class NativeTransformer(nn.Module):
 
     def forward(self, x: Tensor, mask: Tensor) -> Tensor:
         emb = self.embed(x)
-        mask = mask[:, None, :, :].expand(-1, self.nheads, -1, -1).reshape(-1, self.context, self.context)
+        mask = (
+            mask[:, None, :, :]
+            .expand(-1, self.nheads, -1, -1)
+            .reshape(-1, self.context, self.context)
+        )
         out = self.transformer(emb, (~(mask.bool())))
         return self.to_vocab(out)
